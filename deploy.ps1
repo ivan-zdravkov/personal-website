ff7dee267b1a56813de5fc6f2589a8f003239497
i-z <IvanZdravkovBG@gmail.com>
2019-11-03 00:06:52 +0200

    Full Deploy Successful!

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,21 @@
 $server = "ec2-18-196-2-127.eu-central-1.compute.amazonaws.com"
 $accountName = "Administrator"
+$zip = "deploy.zip"
 $origin = Get-Location
-$originZip = "$origin\deploy.zip"
-$destination = "C:\www\ivan"
-$destinationZip = "$destination\deploy.zip"
+$originZip = "$origin\$zip"
+$destination = "C:\www"
+$destinationDir = "$destination\ivan"
+$destinationZip = "$destination\$zip"
 
-Remove-Item -Path $originZip
 Compress-Archive -Path $origin -DestinationPath $originZip
 
 $so = New-PsSessionOption -SkipCACheck -SkipCNCheck
 $creds = Get-Credential -Credential $accountName
 $session = New-PSSession -ComputerName $server -Credential $creds -UseSSL -SessionOption $so
 
-Invoke-Command -Session $session -Command { Remove-Item $Using:destination -Force -Recurse }
-Copy-Item $originZip $destinationZip -ToSession $session -Recurse
+Invoke-Command -Session $session -Command { Remove-Item $Using:destinationDir -Force -Recurse }
+Copy-Item $originZip $destinationZip -ToSession $session -Recurse -Force
+Invoke-Command -Session $session -Command { Expand-Archive $using:destinationZip -DestinationPath $using:destination }
+Invoke-Command -Session $session -Command { Remove-Item $Using:destinationZip -Force -Recurse }
+
+Remove-Item -Path $originZip
