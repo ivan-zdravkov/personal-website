ffe3564dfb5e6e651e225da0939f8b02f5e8584e
i-z <IvanZdravkovBG@gmail.com>
2019-11-02 23:50:55 +0200

    Remove all old data.

--- deploy.ps1
+++ deploy.ps1
@@ -5,11 +5,12 @@ $originZip = "$origin\deploy.zip"
 $destination = "C:\www\ivan"
 $destinationZip = "$destination\deploy.zip"
 
-
 Remove-Item -Path $originZip
 Compress-Archive -Path $origin -DestinationPath $originZip
 
 $so = New-PsSessionOption -SkipCACheck -SkipCNCheck
 $creds = Get-Credential -Credential $accountName
 $session = New-PSSession -ComputerName $server -Credential $creds -UseSSL -SessionOption $so
+
+Invoke-Command -Session $session -Command { Remove-Item $Using:destination -Force -Recurse }
 Copy-Item $originZip $destinationZip -ToSession $session -Recurse
