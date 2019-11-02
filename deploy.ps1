55fe124802bfdb10ca46624f080a9128df2ec74c
i-z <IvanZdravkovBG@gmail.com>
2019-11-02 23:36:12 +0200

    Deploy to remote server.

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,15 @@
 $server = "ec2-18-196-2-127.eu-central-1.compute.amazonaws.com"
 $accountName = "Administrator"
 $origin = Get-Location
+$originZip = "$origin\deploy.zip"
 $destination = "C:\www\ivan"
+$destinationZip = "$destination\deploy.zip"
+
+
+Remove-Item -Path $originZip
+Compress-Archive -Path $origin -DestinationPath $originZip
+
 $so = New-PsSessionOption -SkipCACheck -SkipCNCheck
 $creds = Get-Credential -Credential $accountName
 $session = New-PSSession -ComputerName $server -Credential $creds -UseSSL -SessionOption $so
-Copy-Item $origin $destination -ToSession $session -Recurse
+Copy-Item $originZip $destinationZip -ToSession $session -Recurse
